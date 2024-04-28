064a21e0061b96881aa94741c5ff3e303a30754e
labhvam5 <labhvamkrsharma@gmail.com>
2024-04-28 14:26:01 +0530

    adding models

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from products.models import Products
+from django.forms.models import model_to_dict
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 # Create your views here.
 
+@api_view(["GET"])
 def api_home(request):
-    return JsonResponse({"message": "world is hello world"})
+    prod = Products.objects.all()[0]
+    if prod:
+        res = model_to_dict(prod)
+    else:
+        res = {"error": "Product not found"}
+        Response(res, status=500)
+
+    return Response(res, status=HTTP_500_INTERNAL_SERVER_ERROR)
